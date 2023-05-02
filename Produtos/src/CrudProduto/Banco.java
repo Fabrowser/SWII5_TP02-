c2e89cde92fb06eab9faeb28f7ab423bb1ed2632
Fabricio Guimarães <79614829+Fabrowser@users.noreply.github.com>
2023-05-01 23:26:21 -0300

    Add files via upload

---
+++ Produtos/src/CrudProduto/Banco.java
@@ -0,0 +1,84 @@
+
+/*
+  Matéria:SWII5 – ADS 571 - TP02
+  FABRICIO CARLOS GUIMARAES CB3014886
+  BENILDES FERNANDES DE MENEZES CB 3004392
+  
+ */
+
+package CrudProduto;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Banco {
+
+	private static List<Produto> lista = new ArrayList<>();
+	private static Integer chaveSequencial = 1;
+
+	static {
+
+		Produto produto = new Produto();
+		produto.setId(chaveSequencial++);
+		produto.setNome("ARROZ CAMIL");
+		produto.setUnidadeCompra(3);
+		produto.setDescricao("Arroz 5 KG integral");
+		produto.setQtPrevistoMes(4);
+		produto.setPrecoMaxComprado(20.00);
+						
+		
+		Produto produto2 = new Produto();
+		produto2.setId(chaveSequencial++);
+		produto2.setNome("FEIJAO DE CORDA");
+		produto2.setUnidadeCompra(5);
+		produto2.setDescricao("Feijão NAMORADO 1 KG");
+		produto2.setQtPrevistoMes(7);
+		produto2.setPrecoMaxComprado(12.00);
+			
+		lista.add(produto);
+		lista.add(produto2);
+
+	}
+
+	public void adiciona(Produto produto) {
+		produto.setId(Banco.chaveSequencial++);
+		Banco.lista.add(produto);
+	}
+
+	public List<Produto> getProdutos() {
+
+		return Banco.lista;
+	}
+
+	public void removeProduto(Integer id) {
+
+		Iterator<Produto> it = lista.iterator();
+
+		while (it.hasNext()) {
+			Produto prod = it.next();
+
+			if (prod.getId() == id) {
+
+				it.remove();
+
+			}
+
+		}
+
+	}
+	
+	public Produto buscaProdutoPelaId(Integer id) {
+		
+		for(Produto produto : lista) {
+			if(produto.getId() == id) {
+				return produto;
+			}
+		}
+		
+		return null;
+		
+	}
+	
+	
+}
---
+++ Produtos/src/CrudProduto/Produto.java
@@ -0,0 +1,66 @@
+
+/*
+  Matéria:SWII5 – ADS 571 - TP02
+  FABRICIO CARLOS GUIMARAES CB3014886
+  BENILDES FERNANDES DE MENEZES CB 3004392
+  
+ */
+
+package CrudProduto;
+
+import java.util.Date;
+
+public class Produto {
+	
+	private Integer id; 
+	private String nome;
+	private Integer unidadeCompra;
+	private String descricao; 
+	private Integer qtPrevistoMes; 
+	private Double precoMaxComprado;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public Integer getUnidadeCompra() {
+		return unidadeCompra;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public Integer getQtPrevistoMes() {
+		return qtPrevistoMes;
+	}
+	public Double getprecoMaxComprado() {
+		return precoMaxComprado;
+	}
+	
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setUnidadeCompra(Integer unidadecompra) {
+		this.unidadeCompra = unidadecompra;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public void setQtPrevistoMes(Integer qtprevistomes) {
+		this.qtPrevistoMes = qtprevistomes;
+	}
+	
+	public void setPrecoMaxComprado(Double precomaxcomprado) {
+		this.precoMaxComprado = precomaxcomprado;
+	}
+	
+
+}
